d83227d9c5e4cfb6d57813b6b4835ffe81b0395c
Vicente <65375288+vicentefarias@users.noreply.github.com>
2022-12-08 16:08:46 -0800

    Add files via upload

--- benchmark.cc
+++ benchmark.cc
@@ -10,6 +10,19 @@ using std::cerr;
 using std::chrono;
 
 
+void edge_detect_benchmark(){
+    Image img1 = Image("sp500.png");
+    auto totalTime = 0;
+    for(int i=0; i<5000; i++){
+        auto start = high_resolution_clock::now();
+        img1 = img1.edge_detect(100,200); 
+        auto end = chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        totalTime += time.count();
+    }
+     cout << "totalTime in ms" << (totalTime/5000.0)/1000.0 << std::endl;
+}
+
 int main() {
 
     // time each individual image and video processing function
@@ -25,18 +38,9 @@ int main() {
     //      Load video -> end
     //      ..
 
-    Image img1 = Image("control_img1.jpeg");
-    Image img2 = Image("control_img2.jpeg");
 
-    // test Edge 
-    auto start = high_resolution_clock::now();
-    Image ED = img1.edge_detect(100, 200);
-    auto stop = high_resolution_clock::now();
+// Image Functionalities
 
-    auto start = high_resolution_clock::now();
-    Image ED = img1.gaussian_blur(100);
-    auto stop = high_resolution_clock::now();
 
-    Video cap = Video("control_cap1.jpeg")
-
-}
+    // test load speeds
+  }
